e23536fbff5632adbaa0b22644c10580c06afd1b
Levy Borges <levyborges007@gmail.com>
2025-11-04 15:31:21 -0300

    refactor(register): remove state and city fields from use case interface and zod validation

--- src/http/controllers/orgs/register.ts
+++ src/http/controllers/orgs/register.ts
@@ -7,23 +7,21 @@ export async function register(req: FastifyRequest, reply: FastifyReply ){
     const registerOrgSchema = z.object({
         email: z.string().email(),
         password_hash: z.string().max(50, {message: "maximum characters for password exceeded"}),
+        cep:z.string().max(20, {message: "maximum characters for cep exceeded"}),
         address: z.string().max(100, {message: "maximum characters for address exceeded"}),
         phone_number: z.string().max(20, {message: "maximum characters for phone number exceeded"}),
-        city: z.string().max(100, {message: "maximum characters for city exceeded"}),
-        State: z.string().max(100, {message: "maximum characters for state exceeded"})
     })
    
-const { email, password_hash, address, phone_number, city, State } = registerOrgSchema.parse(req.body)
+const { email, password_hash, address, cep, phone_number} = registerOrgSchema.parse(req.body)
     
    try {
     const registerOrg = makeCreateOrgUseCase()
     await registerOrg.execute({
         email,
         password_hash,
+        cep,
         address,
         phone_number,
-        city,
-        State
     })
    } catch (error) {
     if (error instanceof OrgAlreadyExistsError){
--- src/services/use-cases/orgs/register.ts
+++ src/services/use-cases/orgs/register.ts
@@ -6,10 +6,9 @@ import { hash } from "bcryptjs";
 interface RegisterUseCaseRequest {
     email: string;
     password_hash: string;
+    cep: string;
     address: string;
     phone_number: string;
-    city: string;
-    State: string
 }
 
 interface RegisterUseCaseResponse {
@@ -27,10 +26,10 @@ constructor(
 async execute({
     email, 
     password_hash, 
+    cep,
     address, 
     phone_number, 
-    city, 
-    State} : RegisterUseCaseRequest): Promise<RegisterUseCaseResponse>{
+  } : RegisterUseCaseRequest): Promise<RegisterUseCaseResponse>{
     
     const passwordHash = await hash(password_hash, 6)
 
@@ -43,10 +42,9 @@ async execute({
     const org  = await this.orgsRepostory.create({
         email,
         password_hash: passwordHash,
+        cep,
         address,
         phone_number,
-        city,
-        State
     })
 
     return{
